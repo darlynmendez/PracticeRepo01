97df6b5be213c10f69d551ac414b7bd251ec4b68
darlynmendez <77795140+darlynmendez@users.noreply.github.com>
2021-02-13 15:35:35 -0500

    Updates
    Java student class

---
+++ Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    public static void main(String[] args) {
+        Shape myCalculate = new Shape();
+        myCalculate.getUserInput();
+        myCalculate.sum();
+        myCalculate.product();
+        myCalculate.average();
+        myCalculate.largest();
+        myCalculate.smallest();
+
+        System.out.println("Calculator");
+        System.out.println("Is");
+        System.out.println("Working");
+        System.out.println("On");
+        System.out.println("Calculations");
+    }
+}
